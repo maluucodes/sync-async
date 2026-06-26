310eab4cdfba0e4c784d5ee86d1a8deb5ccc8481
Maria Luiza <m.luiza0020@gmail.com>
2026-06-26 15:36:10 -0300

    initial commit

---
+++ asyncviews/views.py
@@ -0,0 +1,18 @@
+import asyncio
+import httpx
+from django.http import HttpResponse
+
+
+async def http_call_async():
+    for num in range(1, 6):
+        await asyncio.sleep(1)
+        print(num)
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get("https://httpbin.org/")
+        print(response.status_code)
+
+
+async def async_view(request):
+    asyncio.create_task(http_call_async())
+    return HttpResponse("Async started!")
